edafa5ba1e8a6ec7b034f374a7604f83a69c1e8b
shohan-ch <shohan889@yahoo.com>
2023-03-08 02:16:54 +0600

    code formatted

--- server.js
+++ server.js
@@ -6,27 +6,7 @@ const {parse}  = require('querystring')
 const handleRequest  = (req, res)=>{
      const {url}  = req;
 
-
-    if(url=='/formSubmit' && req.method == "GET"){
-      res.end("Form data get method")
-     }
-
-     if(url=='/formSubmit' && req.method == "POST"){
-        let body  =  "";
-        req.on('data', (data)=>{
-            body += data.toString();
-        })
-        req.on('end', ()=>{
-            const formData =  parse(body)
-            console.log(formData.email);
-        })
-
-     }
-
-     if(url=='/name'){
-        const data  = JSON.stringify(name.name)
-        res.end(data)
-     }
+     // Html File show by fs module 
      if(url == '/'){
      file.readFile('./html/index.html', (err, data)=>{
         if(err){
@@ -39,6 +19,31 @@ const handleRequest  = (req, res)=>{
     })
 
 }
+
+    if(url=='/formSubmit' && req.method == "GET"){
+      res.end("Form data get method")
+     }
+
+    //  Form submit from index.html file
+     if(url=='/formSubmit' && req.method == "POST"){
+        let body  =  "";
+        req.on('data', (data)=>{
+            body += data.toString();
+        })
+        req.on('end', ()=>{
+            const formData =  parse(body)
+            console.log(formData);
+        })
+
+     }
+
+     if(url=='/name'){
+        const data  = JSON.stringify(name.name)
+        res.end(data)
+     }
+
+
+
 }
 
 http.createServer(handleRequest).listen(5000)
