25903eea06d862be5b42601c862ef5f0a7fc3e9d
shohan-ch <shohan889@yahoo.com>
2023-03-17 23:53:39 +0600

    Page return from controller

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,6 +1,8 @@
-module.exports = ()=>{
-
+exports.home = ()=>{
     return `<h1> I am from Controllers</h1>`;
+}
 
+exports.article  = (req, res)=>{
 
+    res.end(`<h1>Article Pagefsd</h1>`)
 }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,15 @@
-const Home = require("../controllers/Home");
+const HomeController = require("../controllers/Home");
 
 const routes = (req, res) =>{
     const{url} = req;
 
     if(url=="/"){
-        // const home  =  Home();
-        
-        res.end(Home())
-       
-        // res.end(
-        //     `<h1> This home page </h1> 
-        //     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, pariatur?</p>
-        //     `
-        // )
+        const home  =  HomeController.home();
+        res.end(home)
+    
     }
+
+    if(url=="/article")HomeController.article(req,res)
 
 }
 exports.routes =  routes
